71cbb94336d9f6933987b1091c8390674ba01677
Mike Dark <agyemankojo47@gmail.com>
2025-03-25 23:57:49 +0000

    Format

--- client/src/app/products/page.tsx
+++ client/src/app/products/page.tsx
@@ -9,131 +9,125 @@ import CreateProductModal from "./CreateProductModal";
 import Image from "next/image";
 
 type ProductFormData = {
-  name: string;
-  price: number;
-  stockQuantity: number;
-  rating: number;
-  category: string;
-  image: File | null;
+ name: string;
+ price: number;
+ stockQuantity: number;
+ rating: number;
+ category: string;
+ image: File | null;
 };
 
 const Products = () => {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [isModalOpen, setIsModalOpen] = useState(false);
+ const [searchTerm, setSearchTerm] = useState("");
+ const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const {
-    data: products,
-    isLoading,
-    isError,
-  } = useGetProductsQuery(searchTerm);
+ const { data: products, isLoading, isError } = useGetProductsQuery(searchTerm);
 
-  const [createProduct] = useCreateProductMutation();
+ const [createProduct] = useCreateProductMutation();
 
-  const handleCreateProduct = async (productData: ProductFormData) => {
-    try {
-      // Create FormData for multipart upload
-      const formData = new FormData();
-      // Append text fields
-      formData.append('name', productData.name);
-      formData.append('price', productData.price.toString());
-      formData.append('stockQuantity', productData.stockQuantity.toString());
-      formData.append('rating', productData.rating.toString());
-      formData.append('category', productData.category);
+ const handleCreateProduct = async (productData: ProductFormData) => {
+  try {
+   // Create FormData for multipart upload
+   const formData = new FormData();
+   // Append text fields
+   formData.append("name", productData.name);
+   formData.append("price", productData.price.toString());
+   formData.append("stockQuantity", productData.stockQuantity.toString());
+   formData.append("rating", productData.rating.toString());
+   formData.append("category", productData.category);
 
-      console.log('this is image url',productData.image);
+   console.log("this is image url", productData.image);
 
-      // Append image if exists
-      if (productData.image) {
-        formData.append('image', productData.image);
-      }
+   // Append image if exists
+   if (productData.image) {
+    formData.append("image", productData.image);
+   }
 
-      // Send the FormData
-      await createProduct(formData);
-      setIsModalOpen(false);
-    } catch (error) {
-      console.error("Failed to create product", error);
-    }
-  };
-
-  if (isLoading) {
-    return <div className="py-4">Loading...</div>;
+   // Send the FormData
+   await createProduct(formData);
+   setIsModalOpen(false);
+  } catch (error) {
+   console.error("Failed to create product", error);
   }
+ };
 
-  if (isError || !products) {
-    return (
-      <div className="text-center text-red-500 py-4">
-        Failed to fetch products
-      </div>
-    );
-  }
-  
+ if (isLoading) {
+  return <div className="py-4">Loading...</div>;
+ }
 
+ if (isError || !products) {
   return (
-    <div className="mx-auto pb-5 w-full">
-      {/* SEARCH BAR */}
-      <div className="mb-6">
-        <div className="flex items-center border-2 border-gray-200 rounded">
-          <SearchIcon className="w-5 h-5 text-gray-500 m-2" />
-          <input
-            className="w-full py-2 px-4 rounded bg-white"
-            placeholder="Search products..."
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-        </div>
-      </div>
-
-      {/* HEADER BAR */}
-      <div className="flex justify-between items-center mb-6">
-        <Header name="Products" />
-        <button
-          className="flex items-center bg-blue-500 hover:bg-blue-700 text-gray-200 font-bold py-2 px-4 rounded"
-          onClick={() => setIsModalOpen(true)}
-        >
-          <PlusCircleIcon className="w-5 h-5 mr-2 !text-gray-200" /> Create
-          Product
-        </button>
-      </div>
-
-      {/* BODY PRODUCTS LIST */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 justify-between">
-        {products?.map((product) => (
-          <div
-            key={product.productId}
-            className="border shadow rounded-md p-4 max-w-full w-full mx-auto"
-          >
-            <div className="flex flex-col items-center">
-              <Image
- src={product.image ? `http://localhost:8000${product.image}` : "/assets/default-image.png"}
-                alt={product.name}
-                width={150}
-                height={150}
-                className="mb-3 rounded-2xl w-36 h-36"
-                onError={(e) => {
-                  (e.target as HTMLImageElement).src = '/assets/default-image.png'; // Fallback in case image fails
-                }}
-              />
-              <h3 className="text-lg text-gray-900 font-semibold">
-                {product.name}
-              </h3>
-              <p className="text-gray-800">${product.price.toFixed(2)}</p>
-              <div className="text-sm text-gray-600 mt-1">
-                Stock: {product.stockQuantity}
-              </div>
-              <Rating rating={product.rating ?? 0} />
-            </div>
-          </div>
-        ))}
-      </div>
-
-      {/* CREATE PRODUCT MODAL */}
-      <CreateProductModal
-        isOpen={isModalOpen}
-        onClose={() => setIsModalOpen(false)}
-        onCreate={handleCreateProduct}
-      />
-    </div>
+   <div className="text-center text-red-500 py-4">Failed to fetch products</div>
   );
+ }
+
+ return (
+  <div className="mx-auto pb-5 w-full">
+   {/* SEARCH BAR */}
+   <div className="mb-6">
+    <div className="flex items-center border-2 border-gray-200 rounded">
+     <SearchIcon className="w-5 h-5 text-gray-500 m-2" />
+     <input
+      className="w-full py-2 px-4 rounded bg-white"
+      placeholder="Search products..."
+      value={searchTerm}
+      onChange={(e) => setSearchTerm(e.target.value)}
+     />
+    </div>
+   </div>
+
+   {/* HEADER BAR */}
+   <div className="flex justify-between items-center mb-6">
+    <Header name="Products" />
+    <button
+     className="flex items-center bg-blue-500 hover:bg-blue-700 text-gray-200 font-bold py-2 px-4 rounded"
+     onClick={() => setIsModalOpen(true)}
+    >
+     <PlusCircleIcon className="w-5 h-5 mr-2 !text-gray-200" /> Create Product
+    </button>
+   </div>
+
+   {/* BODY PRODUCTS LIST */}
+   <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 justify-between">
+    {products?.map((product) => (
+     <div
+      key={product.productId}
+      className="border shadow rounded-md p-4 max-w-full w-full mx-auto"
+     >
+      <div className="flex flex-col items-center">
+       <Image
+        src={
+         product.image
+          ? `http://localhost:8000${product.image}`
+          : "/assets/default-image.png"
+        }
+        alt={product.name}
+        width={150}
+        height={150}
+        className="mb-3 rounded-2xl w-36 h-36 object-contain"
+        onError={(e) => {
+         (e.target as HTMLImageElement).src = "/assets/default-image.png"; // Fallback in case image fails
+        }}
+       />
+       <h3 className="text-lg text-gray-900 font-semibold">{product.name}</h3>
+       <p className="text-gray-800">${product.price.toFixed(2)}</p>
+       <div className="text-sm text-gray-600 mt-1">
+        Stock: {product.stockQuantity}
+       </div>
+       <Rating rating={product.rating ?? 0} />
+      </div>
+     </div>
+    ))}
+   </div>
+
+   {/* CREATE PRODUCT MODAL */}
+   <CreateProductModal
+    isOpen={isModalOpen}
+    onClose={() => setIsModalOpen(false)}
+    onCreate={handleCreateProduct}
+   />
+  </div>
+ );
 };
 
 export default Products;
